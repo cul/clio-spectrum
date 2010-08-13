13f4c039c6363d9c56d2d210b51288f21718cc57
James Stuart <tastyhat@jamesstuart.org>
2010-08-13 16:30:48 -0400

    fixed stylesheet errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 require 'vendor/plugins/blacklight/app/helpers/application_helper.rb'
-require 'vendor/plugins/blacklight_advanced_search/app/helpers/application_helper.rb'
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 end
