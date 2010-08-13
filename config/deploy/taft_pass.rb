9a2d348dc1fd248aaa7ef3d4b8a87dd2b1e1f110
James Stuart <tastyhat@jamesstuart.org>
2010-08-13 14:57:12 -0400

    changed deploy to deploy off taft_pass_test

--- config/deploy/taft_pass.rb
+++ config/deploy/taft_pass.rb
@@ -1,9 +1,9 @@
-set :rails_env, "pass_test"
+set :rails_env, "taft_pass"
 set :application, "newbooks_test"
 set :domain,      "taft.cul.columbia.edu"
 set :deploy_to,   "/opt/passenger/#{application}/"
 set :user, "deployer"
-set :branch, "master"
+set :branch, "taft_pass_test"
 set :scm_passphrase, "Current user can full owner domains."
 
 role :app, domain
