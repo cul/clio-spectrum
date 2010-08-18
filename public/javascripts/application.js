d82688a0b8cd6bb14fc392f4d6c71b1b4ec47c85
James Stuart <tastyhat@jamesstuart.org>
2010-08-18 17:09:44 -0400

    modified availability check to use an external javascript function
    have included public/javascripts/application.js and pushed the code
    into that for doing an update_holdings_info.
    
    also removed the prototype files, which were sticking around for no good
    reason.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function update_holdings_info(url) {
+  $(document).ready( function() {
+    $.getJSON(url, function(data) {
+      for (key in data.holdingsId) {
+        selector = "img.availability.holding_" + key;  
+        $(selector).attr("src","images/icons/"+data.holdingsId[key]+".png");
+      }
+    });
+  });
+}
