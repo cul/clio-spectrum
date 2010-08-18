de66bf63ecad7a11d995a173b049c1389c144c74
James Stuart <tastyhat@jamesstuart.org>
2010-08-18 16:44:38 -0400

    modified availability check to make separate calls per item
    run one json call per item that has availability to check rather
    than one big batch call. this dramatically improves the user
    experience.
    
    shoves a lot of JS into the header: am going to trim that down right
    now.

--- app/controllers/backend_controller.rb
+++ app/controllers/backend_controller.rb
@@ -1,18 +1,24 @@
 class BackendController < ApplicationController
   def opac_holdings_data
+    start = Time.now
+
     holdings_url = "http://bearberry.cc.columbia.edu:7014/vxws/GetHoldingsService"
     results = Hash.arbitrary_depth
     
     bibids = params["bibid"].listify
 
-    hc = HTTPClient.new
-    
 
+    hc = HTTPClient.new
+    hc.set_cookie_store(RAILS_ROOT+"/tmp/cookies/holding_cookies.dat")
+
+    logger.debug(Time.now-start)
     bibids.each do |bibid|
       begin
         any_available = false
 
+        logger.info("retrieving #{holdings_url}?bidId=#{bibid}")
         xml = Nokogiri::XML(hc.get_content(holdings_url, :bibId => bibid))
+        logger.debug(Time.now-start)
         xml.root.add_namespace_definition("hol", "http://www.endinfosys.com/Voyager/holdings")
         xml.root.add_namespace_definition("mfhd", "http://www.endinfosys.com/Voyager/mfhd")
         xml.root.add_namespace_definition("item", "http://www.endinfosys.com/Voyager/item")
@@ -29,8 +35,9 @@ class BackendController < ApplicationController
       end
     end
 
-
+    hc.cookie_manager.save_all_cookies(true)
     render :json => results
+
   
   end
 
