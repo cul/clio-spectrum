8a9d73f5b60c59c2ace5a343fe0f4181e576f072
James Stuart <tastyhat@jamesstuart.org>
2010-08-23 14:33:08 -0400

    fixed typo on holdings ajax request

--- config/deploy/taft_pass.rb
+++ config/deploy/taft_pass.rb
@@ -3,7 +3,7 @@ set :application, "newbooks_test"
 set :domain,      "taft.cul.columbia.edu"
 set :deploy_to,   "/opt/passenger/#{application}/"
 set :user, "deployer"
-set :branch, defined?(branch) ? branch : "taft_pass_test"
+set :branch, @variables[:branch] || "taft_pass_test"
 set :scm_passphrase, "Current user can full owner domains."
 
 role :app, domain
