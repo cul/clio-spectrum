6a23e8878f8a874715f22f19bbe709f619979db6
James Stuart <tastyhat@jamesstuart.org>
2010-08-23 15:40:39 -0400

    replaced concurrent requests for holdings with one loop.
    copied _document_list.html.erb locally, added a loop at the end that
    builds an update_holdings_info function with all the ids in one request
    
    pushed $document.ready out of the function, into the javascript block

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function update_holdings_info(url) {
-  $(document).ready( function() {
-    $.getJSON(url, function(data) {
+function update_holdings_info(url,bibids) {
+  for (bibid in bibids) {
+    $.getJSON(url + "?bibid=" + bibids[bibid], function(data) {
       for (key in data.holdingsId) {
         selector = "img.availability.holding_" + key;  
         $(selector).attr("src", RAILS_ROOT + "/images/icons/"+data.holdingsId[key]+".png");
       }
     });
-  });
+  }
 }
