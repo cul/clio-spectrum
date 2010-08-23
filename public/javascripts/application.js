e992ca1249787a574d3c4020b986a751b6a8c26e
James Stuart <tastyhat@jamesstuart.org>
2010-08-23 15:56:04 -0400

    replaced piece by piece json call with one big call to opac_holdings_data

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,12 +2,16 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 function update_holdings_info(url,bibids) {
-  for (bibid in bibids) {
-    $.getJSON(url + "?bibid=" + bibids[bibid], function(data) {
-      for (key in data.holdingsId) {
-        selector = "img.availability.holding_" + key;  
-        $(selector).attr("src", RAILS_ROOT + "/images/icons/"+data.holdingsId[key]+".png");
-      }
-    });
+  bibidstring = '?'
+
+  for (i in bibids) {
+    bibidstring += "bibid[]=" + bibids[i] + "&";
   }
+  
+  $.getJSON(url + bibidstring, function(data) {
+    for (key in data.holdingsId) {
+      selector = "img.availability.holding_" + key;  
+      $(selector).attr("src", RAILS_ROOT + "/images/icons/"+data.holdingsId[key]+".png");
+    }
+  });
 }
