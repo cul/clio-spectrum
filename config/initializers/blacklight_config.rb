757a3e97800302a2e9e3ed874d6b4b9c1aca5a66
James Stuart <tastyhat@jamesstuart.org>
2010-10-27 09:04:40 -0400

    removed bookmark control, aligned css better

--- config/initializers/blacklight_config.rb
+++ config/initializers/blacklight_config.rb
@@ -35,7 +35,7 @@ Blacklight.configure(:shared) do |config|
   
   config[:default_solr_params] = {
     :qt => "search",
-    :per_page => 10 
+    :per_page => 15 
   }
   
   
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,37 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+
+//function for adding items to your folder with Ajax
+$(document).ready(function() {
+	// each form for adding things into the folder.
+	$("form.addFolder, form.deleteFolder").each(function() {
+		var form = $(this);
+    // We wrap the control on the folder page w/ a special element classed so we know not to
+		// attach the jQuery function.  The reason is we want the solr response to refresh so that
+		// pagination as properly udpated.
+		if(form.parent(".in_folder").length == 0){
+			form.children("a.folder_link_submit").click(function() {
+				$.post(form.attr("action") + '?id=' + form.children("input[name=id]").attr("value"), function(data) {
+					var title = form.attr("title");
+					var folder_num, notice_text, new_form_action, new_button_text
+					if(form.attr("action") == "/folder/destroy") {
+						folder_num = parseInt($("#folder_number").text()) - 1;
+						notice_text = title + " removed from your folder."
+						new_form_action = "/folder";
+						new_button_text = "Add to folder"
+					}else{
+						folder_num = parseInt($("#folder_number").text()) + 1
+						notice_text = title + " added to your folder.";
+						new_form_action = "/folder/destroy";
+						new_button_text = "Remove from folder";
+					}
+				  $("#folder_number").text(folder_num);
+					form.attr("action",new_form_action);
+					form.children("a.folder_link_submit").text(new_button_text);
+				});
+				return false;
+			});
+	  }
+	});	
+});
 
 function update_holdings_info(url,bibids) {
   bibidstring = '?'
