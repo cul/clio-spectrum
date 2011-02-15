e9178abf5f22e2844af561346a322f753d5aeaac
James Stuart <tastyhat@jamesstuart.org>
2011-02-15 11:54:02 -0500

    trying taft with username/password

--- app/models/feedback_notifier.rb
+++ app/models/feedback_notifier.rb
@@ -1,8 +1,8 @@
 class FeedbackNotifier < ActionMailer::Base
   def send_feedback(params)
-    from "clio_new_arrivals@columbia.edu"
+    from "clio-new-arrivals-feedback@libraries.cul.columbia.edu"
     subject "New Arrivals Feedback from #{params["email"]}"
-    recipients "james.stuart@gmail.com"
+    recipients "clio-new-arrivals-feedback@libraries.cul.columbia.edu"
     
     @params = params
   end  
