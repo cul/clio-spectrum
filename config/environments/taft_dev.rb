3161280aa8f84f1de06de464b25f289adca74083
James Stuart <tastyhat@jamesstuart.org>
2011-02-15 11:41:57 -0500

    added taft_dev smtp

--- config/environments/taft_dev.rb
+++ config/environments/taft_dev.rb
@@ -16,5 +16,12 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
-config.action_mailer.raise_delivery_errors = false
+
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :address => "localhost",
+  :domain => "taft.cul.columbia.edu",
+  :port => 25
+}
 
