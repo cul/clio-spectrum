e1be3cde46051bec13bb94cb2483938621bd1536
James Stuart <github@jamesstuart.org>
2011-05-11 16:32:57 -0400

    right after rails update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewBooks::Application.config.secret_token = '30efc4e7909c4d8cb8998f0f242c4f91077bf906dcdcd56fee3daee7cefc6fcb51763c31cd74383e62ddb6a98a1f47206992ca9b8697710f2c896dc60f34ed22'
