d26299dd7a574a0bbeb42a54b323d745c1df352e
James Stuart <github@jamesstuart.org>
2011-05-13 10:59:10 -0400

    fix up passenger environments

--- config/environments/passenger_dev.rb
+++ config/environments/passenger_dev.rb
@@ -1,21 +1,20 @@
-# Settings specified here will take precedence over those in config/environment.rb
 
-# In the development environment your application's code is reloaded on
-# every request.  This slows down response time but is perfect for development
-# since you don't have to restart the webserver when you make code changes.
-config.cache_classes = true
+NewBooks::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb
 
-# Log error messages when you accidentally call methods on nil.
-config.whiny_nils = true
+  config.cache_classes = true
 
-# Show full error reports and disable caching
-config.action_controller.consider_all_requests_local = true
-config.action_view.debug_rjs                         = true
-config.action_controller.perform_caching             = false
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
 
-# Don't care if the mailer can't send
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = false
+  config.action_view.debug_rjs             = false
+  config.action_controller.perform_caching = true
 
-config.action_mailer.raise_delivery_errors = true
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
+
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address => "smtp.gmail.com",
@@ -25,4 +24,9 @@ config.action_mailer.smtp_settings = {
   :user_name => "clio.new.arrivals@gmail.com",
   :password => "qbridge7engage"
 }
+  # Print deprecation notices to the Rails logger
+  config.active_support.deprecation = :log
 
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
+end
--- config/environments/passenger_prod.rb
+++ config/environments/passenger_prod.rb
@@ -1,36 +1,21 @@
 
-# Settings specified here will take precedence over those in config/environment.rb
 
-# The production environment is meant for finished, "live" apps.
-# Code is not reloaded between requests
-config.cache_classes = true
+NewBooks::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb
 
-# Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
-config.action_view.cache_template_loading            = true
+  config.cache_classes = true
 
-# See everything in the log (default is :info)
-# config.log_level = :debug
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
 
-# Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = false
+  config.action_view.debug_rjs             = false
+  config.action_controller.perform_caching = true
 
-# Use a different cache store in production
-# config.cache_store = :mem_cache_store
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
 
-# Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host = "http://assets.example.com"
-
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
-
-# Enable threaded mode
-# config.threadsafe!
-
-
-
-config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address => "smtp.gmail.com",
@@ -40,6 +25,9 @@ config.action_mailer.smtp_settings = {
   :user_name => "clio.new.arrivals@gmail.com",
   :password => "qbridge7engage"
 }
+  # Print deprecation notices to the Rails logger
+  config.active_support.deprecation = :log
 
-# config.logger.level = Logger::WARN
-
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
+end
