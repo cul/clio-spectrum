13f244b4a8c25ba94de52c1fc09108746e1ca870
James Stuart <github@jamesstuart.org>
2011-05-27 10:18:04 -0400

    updated version to 3.0.0rc2

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "lib/james_monkeys"
 require "lib/google_books"
 require "lib/voyager_holding"
 
-RELEASE_STAMP = "2.8.1"
+RELEASE_STAMP = "3.0.0rc2"
 
 module NewBooks
   class Application < Rails::Application
