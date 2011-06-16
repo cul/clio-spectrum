9cd6f1225f1d2d2f0c9b22e15f33d5ad950158f4
James Stuart <github@jamesstuart.org>
2011-06-16 14:30:01 -0400

    fixed problem with locations with periods in them

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ NewBooks::Application.routes.draw do
   devise_for :users
 
   match 'backend/clio_recall/:id', :to => "backend#clio_recall" , :as => :clio_recall
-  match 'locations/show/:id', :to => "locations#show", :as => :location_display
+  match 'locations/show/:id', :id => /[^\/]+/, :to => "locations#show", :as => :location_display
   match 'backend/feedback_mail', :to => "backend#feedback_mail"
   match 'welcome/versions', :to => "welcome#versions"
   namespace :admin do
