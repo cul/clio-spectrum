3e7a34a111fbb7833381b729749532e63d82d945
James Stuart <github@jamesstuart.org>
2011-07-07 11:35:03 -0400

    fixed cross-site scripting problems, upgraded to jquery 1.6.2
    removed render_head_content
    added jquery 1.6.2
    turned off csrf protection for holdings requests

--- public/javascripts/jquery_snippet.js
+++ public/javascripts/jquery_snippet.js
@@ -2,6 +2,9 @@ function CSRFProtection(xhr) {
  var token = $('meta[name="csrf-token"]').attr('content');
  if (token) xhr.setRequestHeader('X-CSRF-Token', token);
 }
-if ('ajaxPrefilter' in $) $.ajaxPrefilter(function(options, originalOptions, xhr) { CSRFProtection(xhr); });
+if ('ajaxPrefilter' in $) $.ajaxPrefilter(function(options, originalOptions, xhr) { 
+ if (options.url.indexOf("holdings/fetch") == -1)
+   CSRFProtection(xhr); });
 else $(document).ajaxSend(function(e, xhr) { CSRFProtection(xhr); });
+
 
