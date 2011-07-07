2c28d3d3ddf073103141de5bf177adb180c70921
James Stuart <github@jamesstuart.org>
2011-07-07 13:10:35 -0400

    updated version 3.0.1, updated deploy scripts

--- config/deploy/na_test.rb
+++ config/deploy/na_test.rb
@@ -1,9 +1,9 @@
-set :rails_env, "passenger_test"
+set :rails_env, "na_test"
 set :application, "new_arrivals_test"
 set :domain,      "rhys.cul.columbia.edu"
 set :deploy_to,   "/opt/passenger/#{application}/"
 set :user, "deployer"
-set :branch, @variables[:branch] || "passenger_test"
+set :branch, @variables[:branch] || "na_test"
 set :scm_passphrase, "Current user can full owner domains."
 
 role :app, domain
