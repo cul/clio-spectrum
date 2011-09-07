21393d81d748cf1c251d8e5c122b1d5a70770ce0
Gary Bertchume <bertchum@columbia.edu>
2011-09-07 17:06:16 -0400

    Structure for both simple and complex holdings
    For simple holdings consolidate status messages; for complex holdings do
    breakdown by copy but still aggregated by location. Also added call
    number to uniqueness check to accommodate multiple holdings with
    different call numbers in the same location.

--- app/helpers/holdings_helper.rb
+++ app/helpers/holdings_helper.rb
@@ -8,9 +8,37 @@ module HoldingsHelper
     rescue
       return []
     end
+    
+    complexity = determine_complexity(holdings)
+    
+    if complexity == :simple
+      process_simple_holdings(holdings)
+    else
+      process_complex_holdings(holdings)
+    end
+    
+  end
+
+  def determine_complexity(holdings)
+    
+    complexity = :simple
+    holdings.records.each do |record|
+      if !record.summary_holdings.empty? ||
+          !record.temp_locations.empty?
+        complexity = :complex
+      end
+    end
+    
+    complexity
+      
+  end
+
+  def process_simple_holdings(holdings)
+    
     entries = [] 
     holdings.records.each do |record|
-      entry = entries.find { |entry| entry[:location_name] == record.location_name }
+      entry = entries.find { |entry| entry[:location_name] == record.location_name && 
+        entry[:call_number] == record.call_number}
 
       unless entry
         location = Location.match_location_text(record.location_name)
@@ -19,7 +47,7 @@ module HoldingsHelper
           :location_name => record.location_name,
           :call_number => record.call_number,
           :location => location,
-          :items => {}
+          :copies => [{:items => {}}]
         }
 
         if location && location.category == "physical"
@@ -36,20 +64,91 @@ module HoldingsHelper
         entries << entry
       end
 
+      # for simple holdings put consolidated status information in the first copy
+
       messages = record.item_status[:messages]
-      if entry[:items].has_key?(messages)
-        entry[:items][messages][:count] += 1
-      else
-        entry[:items][messages] = {
-          :status => record.item_status[:status],
-          :image_link => image_tag("icons/" + record.item_status[:status] + ".png"),
-          :count => 1
-        }
+      messages.each do |message|
+        if entry[:copies][0][:items].has_key?(message)
+          entry[:copies][0][:items][message][:count] += 1
+        else
+          entry[:copies][0][:items][message] = {
+            :status => record.item_status[:status],
+            :image_link => image_tag("icons/" + record.item_status[:status] + ".png"),
+            :count => 1
+          }
+        end
       end
 
     end
 
     entries
+        
+  end
+
+  def process_complex_holdings(holdings)
+    
+    entries = [] 
+    holdings.records.each do |record|
+      entry = entries.find { |entry| entry[:location_name] == record.location_name }
+
+      unless entry
+        location = Location.match_location_text(record.location_name)
+
+        entry = {
+          :location_name => record.location_name,
+          :call_number => record.call_number,
+          :location => location,
+          :copies => []
+        }
+
+        if location && location.category == "physical"
+          check_at = DateTime.now
+          entry[:location_link] = link_to(record.location_name, location_display_path(CGI.escape(record.location_name)), :class => :location_display)
+        else
+          entry[:location_link] = record.location_name
+        end
+
+        if location && location.library && (hours = location.library.hours.find_by_date(Date.today))
+          entry[:hours] = hours.to_opens_closes
+        end
+        
+        entries << entry
+      end
+
+      out = {}
+
+      # process status messages
+      messages = record.item_status[:messages]
+      items = {}
+      messages.each do |message|
+        if items.has_key?(message)
+          items[message][:count] += 1
+        else
+          items[message] = {
+            :status => record.item_status[:status],
+            :image_link => image_tag("icons/" + record.item_status[:status] + ".png"),
+            :count => 1
+          }
+        end
+      end
+      out[:items] = items
+      
+      # summary holdings
+      if !record.summary_holdings.empty?
+        out[:summary_holdings] = "Library has: " + record.summary_holdings.join(' ')
+      end
+      
+      # temp locations
+      if !record.temp_locations.empty?
+        out[:temp_locations] = record.temp_locations
+      end
+      
+      entry[:copies] << out
+
+    end
+    
+    entries
+    
   end
 
 end
