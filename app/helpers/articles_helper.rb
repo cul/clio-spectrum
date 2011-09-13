a1ea77edf7105bd410b4d441f158c276e4ab5fd4
James Stuart <github@jamesstuart.org>
2011-09-13 01:50:24 -0400

    basic display of articles finished

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,4 +3,20 @@ module ArticlesHelper
     link_title ||= article.title.html_safe
     link_to link_title, article_show_path(:openurl => article.src['openUrl'])
   end
+
+  def get_article_citation(doc)
+    results = []
+    results <<  "#{doc.publication_title}"  if doc.publication_title
+    results << "#{process_summon_date(doc.publication_date)}" if doc.publication_date
+    results << "ISSN: #{doc.issns.first}" unless doc.issns.empty?
+    results << "Volume #{doc.volume.to_s}" if doc.volume
+    results << "Issue #{doc.issue}" if doc.issue
+    results << "p. #{doc.start_page}" if doc.start_page 
+
+    results.join(", ")
+  end
+
+  def process_summon_date(date)
+    [date.day, date.month, date.year].compact.join("/")
+  end
 end
