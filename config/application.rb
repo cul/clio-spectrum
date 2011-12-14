f1761bbecbb1eee67d3632976741183b8a6daf43
James Stuart <github@jamesstuart.org>
2011-12-14 10:18:54 -0500

    version update

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require File.expand_path('../../lib/james_monkeys', __FILE__)
 require File.expand_path('../../lib/google_books', __FILE__)
 require File.expand_path('../../lib/voyager_holding', __FILE__)
 
-RELEASE_STAMP = "3.2.2"
+RELEASE_STAMP = "3.2.4"
 
 module NewBooks
   class Application < Rails::Application
