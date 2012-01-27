1312ac951ffa41aabce1c1861f1b8055e68944ec
erik r <er2576@columbia.edu>
2012-01-27 12:25:41 -0500

    font adds. style adds. footer adds.

--- app/assets/javascripts/zzz_cliobeta_er_theme.js
+++ app/assets/javascripts/zzz_cliobeta_er_theme.js
@@ -2,10 +2,10 @@ $(function() {
  if ( $('#sources').height() < $('#bd').height() ) {
 	$('#sources').height($('#bd').height());
  }
-});
+}); //ready
 
 $(window).load(function() {
  if ( $('#sources').height() < $('#bd').height() ) {
 	$('#sources').height($('#bd').height());
  }
-});
+}); //window
