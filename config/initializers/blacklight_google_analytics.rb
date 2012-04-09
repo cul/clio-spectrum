b4daf7477ad061f4b6e78aebe41a7132db4a3719
James Stuart <github@jamesstuart.org>
2012-04-09 12:41:54 -0400

    changed analytics ids

--- config/initializers/blacklight_google_analytics.rb
+++ config/initializers/blacklight_google_analytics.rb
@@ -1,8 +1,10 @@
 # Change to your Google Web id 
 BlacklightGoogleAnalytics.web_property_id = case Rails.env.to_s
-when 'development', 'spectrum_dev', 'spectrum_test'
+when 'development'
+  'UA-30642217-2' 
+when 'spectrum_dev', 'spectrum_test'
   'UA-30642217-1' 
-when  'test'
+when 'test'
   nil
 else
   'UA-28923110-1'
