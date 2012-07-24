01ce43c7be32ac963baec5b41ba80c70e9a07cb5
James Stuart <github@jamesstuart.org>
2012-07-24 16:13:27 -0400

    added author relationship to database alert

--- app/models/database_alert.rb
+++ app/models/database_alert.rb
@@ -1,3 +1,8 @@
 class DatabaseAlert < ActiveRecord::Base
+  belongs_to :author, :class_name => "User", :foreign_key => "author_id"
+
   attr_accessible :active, :author_id, :clio_id, :message
+
+  validates :author_id, :presence => true
+  validates :message, :presence => true
 end
