4d8168677a450343e42324f3bb8f0b1c2fe412ca
James Stuart <github@jamesstuart.org>
2012-10-04 14:27:30 -0400

    bumped voyager_api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,21 +12,16 @@ class ApplicationController < ActionController::Base
   end
 
   def look_up_clio_holdings(documents)
-    puts (start = Time.now.to_f)
     clio_docs = documents.select { |d| d.get('clio_id_display')}
     
     unless clio_docs.empty? 
-      holdings = Voyager::Request.simple_holdings_check(*clio_docs.collect { |cd| cd.get('clio_id_display')})
+      holdings = Voyager::Request.simple_holdings_check(connection_details: APP_CONFIG[:oracle_connection_details] , bibids: clio_docs.collect { |cd| cd.get('clio_id_display')})
       clio_docs.each do |cd|
         cd['clio_holdings'] = holdings[cd.get('clio_id_display')]
       end
     end
 
-    puts (Time.now.to_f - start)
 
-  end
-  def test_notification_error
-    raise "test"
   end
 
   def get_and_debug_search_results(user_params = params || {}, extra_controller_params = {})
