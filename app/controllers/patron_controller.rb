fdc8e895f6f409716ea6b7808d27c9b514afa289
James Stuart <github@jamesstuart.org>
2013-04-15 15:20:59 -0400

    renewal added

--- app/controllers/patron_controller.rb
+++ app/controllers/patron_controller.rb
@@ -10,6 +10,31 @@ class PatronController < ApplicationController
     @voyager_connection = Voyager::Connection.new(APP_CONFIG['voyager_connection'])
     @patron = Voyager::Patron.new(uni: current_user.login, connection: @voyager_connection)
 
+    if params['commit'] && params['loans_renew'] && !params['loans_renew'].empty?
+      @debug_entries['loans'] = []
+      successful = 0
+      total = params['loans_renew'].length
+      params['loans_renew'].each do |db_item_key|
+        resp = @patron.renew_loan(db_item_key)
+        
+        if resp['response']['reply_text'] == 'ok'
+          successful += 1
+        end
+        @debug_entries['loans'] << resp
+       
+      end
+
+      if total == successful
+        flash[:success] = "#{total} renewed successfully."
+      elsif successful > 0
+        flash[:notice] = "#{successful} of #{total} renewed successfully."
+      else
+        flash[:error] = "Renewal failed."
+      end
+
+      @patron.reload_loans!
+    end
+
 
 
   end
