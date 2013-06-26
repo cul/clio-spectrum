5b150d698d90877f99848b885b4c4006b27af420
Stuart Marquis <marquis@columbia.edu>
2013-06-26 16:16:44 -0400

    spec fixes to match updated lweb

--- spec/features/library_hours_spec.rb
+++ spec/features/library_hours_spec.rb
@@ -9,7 +9,7 @@ describe "Item Locations should show correct library hours", :js => true do
     click_link('Avery Drawings & Archives - By appt. (Non-Circulating)')
     # page.save_and_open_page # debug
     page.should have_text('Avery Drawings & Archives')
-    page.should have_link("Full Hours Info", :href=>"http://www.columbia.edu/cu/lweb/services/hours/index.html?library=averydr")
+    page.should have_link("Full Hours Info", :href=>"http://www.columbia.edu/cu/lweb/services/hours/index.html?library=avery-drawings-archives")
   end
 
   it "for Avery Classics" do
@@ -17,7 +17,7 @@ describe "Item Locations should show correct library hours", :js => true do
     page.should have_text('Avery Classics')
     click_link('Avery Classics')
     page.should have_text('Avery Classics')
-    page.should have_link("Full Hours Info", :href=>"http://www.columbia.edu/cu/lweb/services/hours/index.html?library=averycl")
+    page.should have_link("Full Hours Info", :href=>"http://www.columbia.edu/cu/lweb/services/hours/index.html?library=avery-classics")
   end
 
 
@@ -26,8 +26,8 @@ describe "Item Locations should show correct library hours", :js => true do
     page.should have_text('Oral History, 801 Butler')
     click_link('Oral History, 801 Butler')
     page.should have_text('Oral History')
-    page.should have_link("Home Page", :href=>"http://library.columbia.edu/indiv/ccoh.html")
-    page.should have_link("Full Hours Info", :href=>"http://www.columbia.edu/cu/lweb/services/hours/index.html?library=ohro")
+    page.should have_link("Home Page", :href=>"http://library.columbia.edu/locations/ccoh.html")
+    page.should have_link("Full Hours Info", :href=>"http://www.columbia.edu/cu/lweb/services/hours/index.html?library=ccoh")
   end
 
 end
