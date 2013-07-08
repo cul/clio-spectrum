b3395ef4c4d1700495073b626971e77c0137d329
Stuart Marquis <marquis@columbia.edu>
2013-07-08 15:20:39 -0400

    rails migration field length set by "limit", not "length"

--- db/migrate/20130614173638_create_my_list_items.rb
+++ db/migrate/20130614173638_create_my_list_items.rb
@@ -3,13 +3,13 @@ class CreateMyListItems < ActiveRecord::Migration
     create_table :my_list_items do |t|
       t.integer :my_list_id
       t.string :item_source
-      t.string :item_key, :length => 200
+      t.string :item_key, :limit => 200
       t.integer :sort_order
 
       t.timestamps
     end
-    
+
     add_index :my_list_items, [:my_list_id, :item_key], :unique => true
-    
+
   end
 end
