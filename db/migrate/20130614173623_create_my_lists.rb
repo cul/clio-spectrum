8abafe7ec61ea3edde44979016c83edce547a89f
Stuart Marquis <marquis@columbia.edu>
2013-07-08 15:27:08 -0400

    fix again for utf8 mysql sizes

--- db/migrate/20130614173623_create_my_lists.rb
+++ db/migrate/20130614173623_create_my_lists.rb
@@ -1,9 +1,9 @@
 class CreateMyLists < ActiveRecord::Migration
   def change
     create_table :my_lists do |t|
-      t.string :owner, :null => false, :limit => 100
-      t.string :name, :null => false, :limit => 500
-      t.string :slug, :null => false, :limit => 500
+      t.string :owner, :null => false, :limit => 20
+      t.string :name, :null => false, :limit => 200
+      t.string :slug, :null => false, :limit => 200
       t.string :description, :default => ''
       t.string :sort_by
       t.string :permissions, :default => "private"
