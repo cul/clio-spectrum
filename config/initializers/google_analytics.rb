6251b8d3923d58c5c9706a5191a1e699eb60af7f
Stuart Marquis <marquis@columbia.edu>
2014-01-28 12:59:45 -0500

    cleanup, again

--- config/initializers/google_analytics.rb
+++ config/initializers/google_analytics.rb
@@ -1,28 +1,30 @@
 module GoogleAnalytics
 
+  mattr_accessor :web_property_id
+
+end
+
+
 # Change to your Google Web id
-# GoogleAnalytics.web_property_id = case Rails.env.to_s
+GoogleAnalytics.web_property_id = case Rails.env.to_s
 
-web_property_id = case Rails.env.to_s
+  # PROD
+  when 'clio_prod'
+    'UA-28923110-1'
 
-# PROD
-when 'clio_prod'
-  'UA-28923110-1'
+  # TEST
+  when 'clio_test'
+    'UA-28923110-3'
 
-# TEST
-when 'clio_test'
-  'UA-28923110-3'
+  # DEV
+  when 'clio_dev'
+    'UA-28923110-4'
+  # localhost development - send events under "CLIO Dev" profile
+  when 'development'
+    'UA-28923110-4'
 
-# DEV
-when 'clio_dev'
-  'UA-28923110-4'
-# localhost development - send events under "CLIO Dev" profile
-when 'development'
-  'UA-28923110-4'
-
-else
-  nil
-
+  else
+    nil
 end
 
-end
+
