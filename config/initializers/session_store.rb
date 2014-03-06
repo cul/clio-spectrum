abb1a504ceb2b2920f8e1ab15a945970a158aa36
Stuart Marquis <marquis@columbia.edu>
2014-03-06 13:26:09 -0500

    sessions - cookies or database or something else?

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,14 @@
 # Be sure to restart your server when you modify this file.
 
+# 3/2014 - we're seeing a handful of ActionDispatch::Cookies::CookieOverflow
+# errors in production.
+# The session holds the full set of search params (all facet values), plus the
+# previous_url.  Should we revert to ActiveRecord?  Or try file-store?
+
 Clio::Application.config.session_store :cookie_store, :key => '_clio_session'
+# Clio::Application.config.session_store :active_record_store, :key => '_clio_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
+
