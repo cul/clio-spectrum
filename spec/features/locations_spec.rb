37aeb3372696d97cd49e19ac366e7077a1594b7e
Stuart Marquis <marquis@columbia.edu>
2014-10-17 13:26:00 -0400

    NEXT-1129 - Request to change text of NYSPI

--- spec/features/locations_spec.rb
+++ spec/features/locations_spec.rb
@@ -22,4 +22,18 @@ describe 'Locations' do
     page.should_not have_css('.location_notes')
 
   end
+
+
+  # NEXT-1129 - Request to change text of NYSPI
+  it 'should show correct phone for NYS Psychiatric Inst', js: true do
+
+    # The full complete URL
+    visit location_display_path('NYS+Psychiatric+Institute+Library+%28Circulation+Restricted%29')
+    page.should have_text("Call (646) 774 - 8613 between 9-5pm")
+
+    # Test substring matching...
+    visit location_display_path('NYS+Psychiatric+Institute')
+    page.should have_text("Call (646) 774 - 8613 between 9-5pm")
+  end
+
 end
