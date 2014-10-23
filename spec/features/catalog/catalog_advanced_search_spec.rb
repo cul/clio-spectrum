1ff7b37092a14c66491342db54a4ce0863e68184
Stuart Marquis <marquis@columbia.edu>
2014-10-23 13:53:36 -0400

    Specs for location search, and all other advanced search options

--- spec/features/catalog/catalog_advanced_search_spec.rb
+++ spec/features/catalog/catalog_advanced_search_spec.rb
@@ -68,6 +68,68 @@ describe 'Catalog Advanced Search' do
 
   end
 
+  # Test each individual advanced-search option
+  { 'Title' => 'smith',
+    'Journal Title' => 'Journal',
+    'Author' => 'Smith',
+    'Series' => 'Series',
+    # We have some funny quoting logic around this field
+    'Title Begins With' => '"Three"',
+    'Subject' => 'Science',
+    'Form/Genre' => 'Manuscript',
+    'Publication Place' => 'Boston',
+    'Publisher' => 'Penguin',
+    'Publication Year' => '1999',
+    # check for specific bibs?
+    # 'ISBN' => '9780470405543',
+    # 'ISSN' => '2372-5699',
+    # or search by prefix?
+    'ISBN' => '99921*',
+    'ISSN' => '2372',
+    'Call Number' => 'PN1995.9.P7',
+    'Location' => 'Dakhla Library',
+  }.each_pair do |searchField, searchValue|
+
+
+    it "supports fielded search by #{searchField}", js: true, focus: true do
+      visit catalog_index_path
+      find('btn', text: "All Fields").click
+      within('.search_row') do
+        # find('li', text: "Title").click
+        # find("a[data-value='#{searchField}']").click
+        # find('li', text: searchField, exact: true).click
+        find('li', text: /\A#{searchField}\z/).click
+      end
+      fill_in 'q', with: searchValue
+      find('button[type=submit]').click
+
+      find('.constraint-box').should have_content("#{searchField}: #{searchValue}")
+      page.should have_text "« Previous | 1 - 25 of"
+      # Use this block instead if we want to search by specific bibs
+      # if ["ISBN", "ISSN"].include? searchField
+      #   page.should have_text "1 entry found"
+      # else
+      #   page.should have_text "« Previous | 1 - 25 of"
+      # end
+    end
+
+
+    it "supports advanced search by #{searchField}", js: true, focus: true do
+      visit catalog_index_path
+      find('.search_box.catalog .advanced_search_toggle').click
+      within '.landing_page.catalog .advanced_search' do
+        select(searchField, from: 'adv_1_field')
+        fill_in 'adv_1_value', with: searchValue
+        find('button[type=submit]').click
+      end
+      find('.constraint-box').should have_content("#{searchField}: #{searchValue}")
+      page.should have_text "« Previous | 1 - 25 of"
+    end
+
+
+  end
+
+
   # Bug - Dismissing the last advanced-search field should WORK
   # (CatalogController#preprocess_search_params:  undefined method `gsub!' for nil:NilClass)
   it 'should allow dismissing of final advanced fielded search param', js: true do
