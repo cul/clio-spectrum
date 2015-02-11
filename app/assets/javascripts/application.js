f397fabb0f873b1a49cc4b265f7099b4d379dfff
Marian Mosley <marian.mosley@gmail.com>
2015-02-11 11:11:40 -0500

    pull in google js before gmaps js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -55,8 +55,6 @@
 //= require saved_lists
 //= require scroll_memory
 //= require underscore
-//= require gmaps/google
-//= require google_map
 
 
 // was used in support of JavaScript landing-page switching,
