0ecbef7bfa5b0075d811318ef8a9821e8fea2539
Marian Mosley <marian.mosley@gmail.com>
2015-03-10 10:42:45 -0400

    generate ga event when suggestions & feedback is clicked

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,19 +1,3 @@
-
-// OLDER CODE, COPIED FROM _common_head.html.haml
-// 
-// var _gaq = _gaq || [];
-// _gaq.push(['_setAccount', '#{GoogleAnalytics.web_property_id}']);
-// _gaq.push(['_setSiteSpeedSampleRate', 100]);
-// _gaq.push(['_trackPageview']);
-// 
-// (function() {
-//   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-//   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-//   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-// })();
-
-
-
 $(document).ready(function() {
 
   // UNIVERSAL TRACKING CODE FROM GA WEBSITE
@@ -24,10 +8,13 @@ $(document).ready(function() {
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   // Point our running instance to the appropriate GA property definition
-  ga('create', google_analytics_web_property_id, 'columbia.edu');
-
-  // FOR LOCALHOST DEVELOPMENT USE THIS LINE INSTEAD
-  // ga('create', google_analytics_web_property_id, 'none');
+  //
+  if ($("body").data("rails-env") == 'development'){
+    ga('create', google_analytics_web_property_id, 'none');
+  }
+  else{
+    ga('create', google_analytics_web_property_id, 'columbia.edu');
+  }
 
   // This sends the a normal page-view to Google Analytics.
   // It runs once on page load.
@@ -42,58 +29,70 @@ $(document).ready(function() {
   // Apply to all off-site <A> tags, based on:
   //   http://www.electrictoolbox.com/jquery-open-offsite-links-new-window/
 
-  $('a[href]').filter( function() {return this.hostname && this.hostname !== location.hostname} ).each(function() {
+  $(document).on('click', 'a', function(event) {
 
-    // var href   = $(this).attr("href");
-    // var target = $(this).attr("target");
-    // var text   = $(this).text();
-    // console.log("found a.href href=["+href+"] text=["+text+"]")
+    if (this.hostname && this.hostname === location.hostname){
+      if (!($(this).hasClass('dropdown-link'))){
+        return;
+      }
+    }
 
-    $(this).click(function(event) { // when someone clicks these links
-      // Gather up values at time of click, not at first load, to allow
-      // for ajax updates to, e.g., href labels or targets
+    // Gather up values at time of click, not at first load, to allow
+    // for ajax updates to, e.g., href labels or targets
 
-      var href   = $(this).attr("href");
-      var target = $(this).attr("target");
-      var text   = $(this).text();
+    var href   = $(this).attr("href");
+    var target = $(this).attr("target");
+    var text   = $(this).text();
 
-      // The GA Category/Action may be given at a higher DOM level,
-      // e.g., at the root of an html menu/list of links, or a container div,
-      var category = $(this).closest("[data-ga-category]").data("ga-category") || "Outbound Link";
-      var action = $(this).closest("[data-ga-action]").data("ga-action") || "Click";
-      // Should the GA label default to the text or the URL?
-      var label = $(this).data("ga-label") || text;
+    // The GA Category/Action may be given at a higher DOM level,
+    // e.g., at the root of an html menu/list of links, or a container div,
 
+    var category = $(this).closest("[data-ga-category]").data("ga-category") || "Outbound Link";
+    var action = $(this).closest("[data-ga-action]").data("ga-action") || "Click";
+    var label = $(this).data("ga-label") || text;
+
+    var open_new_window = false;
+
+    // Offsite links will open a new window unless it is a download link
+
+    //if (!($(this).hasClass('dropdown-link'))){
+    if ((this.hostname && this.hostname !== location.hostname) && (!($(this).hasClass('download')))){
+      open_new_window = true;
+    }
+
+    if (open_new_window){
       event.preventDefault(); // don't open the link yet
+    }
 
-      // console.log("ga('send','event','"+category+"','"+action+"','"+label+"')")
-      ga('send', 'event', category, action, label);
+    logToConsole("ga('send','event','"+category+"','"+action+"','"+label+"')");
+    ga('send', 'event', category, action, label);
 
+    if (open_new_window){
       setTimeout(function() { // now wait 300 milliseconds...
         window.open(href, (!target ? "_blank" : target)); // ...and open in new blank window
       },300);
-    });
+    }
   });
 
 
   $(this).bind('copy', function() {
     var selectedText = "";
     if (window.getSelection) {
-        selectedText = window.getSelection().toString()
+        selectedText = window.getSelection().toString();
     } else if (document.selection && document.selection.type != "Control") {
-        selectedText = document.selection.createRange().text
+        selectedText = document.selection.createRange().text;
     }
     if (selectedText.length > 0) {
       // console.log('copy event')
       // What GA category/action/label do we want to log this event as?
     }
     return true;
-  }); 
-
+  });
 });
 
 
-
-
-
-
+function logToConsole(message) {
+  if ($("body").data("rails-env") == 'development') {
+    console.log(message)
+  }
+}
