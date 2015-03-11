78604be623ca369a0927fc9953a949a5dded20e8
Marian Mosley <marian.mosley@gmail.com>
2015-03-11 17:20:22 -0400

    get rid of js errors

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -1,25 +1,27 @@
 //= require gmaps/google
 //= require underscore
 
-handler = Gmaps.build('Google');
-var aryMapStyles =  [
-{
-  featureType: "poi.business",
-  elementType: "all",
-  stylers: [
-    {visibility: "off"}
-  ]
+var mapExists = document.getElementById("map"); 
+if(mapExists){
+  handler = Gmaps.build('Google');
+  var aryMapStyles =  [
+  {
+    featureType: "poi.business",
+    elementType: "all",
+    stylers: [
+      {visibility: "off"}
+    ]
+  }
+  ];
+  handler.buildMap({ 
+    provider: {styles:aryMapStyles},
+    internal: {id: 'map'}}, function(){
+    markers = handler.addMarkers($('#map').data('markers'));
+    handler.bounds.extendWith(markers);
+    handler.map.centerOn(new google.maps.LatLng(40.8078425, -73.9621477));
+    handler.getMap().setZoom(17);
+    google.maps.event.trigger(markers[($('#map').data('currentLocationIndex'))].serviceObject, 'click');
+    var transitLayer = new google.maps.TransitLayer();
+    transitLayer.setMap(handler.map.serviceObject);
+  });
 }
-];
-handler.buildMap({ 
-  provider: {styles:aryMapStyles},
-  internal: {id: 'map'}}, function(){
-  markers = handler.addMarkers($('#map').data('markers'));
-  handler.bounds.extendWith(markers);
-  handler.map.centerOn(new google.maps.LatLng(40.8078425, -73.9621477));
-  handler.getMap().setZoom(17);
-  google.maps.event.trigger(markers[($('#map').data('currentLocationIndex'))].serviceObject, 'click');
-  var transitLayer = new google.maps.TransitLayer();
-  transitLayer.setMap(handler.map.serviceObject);
-});
-
