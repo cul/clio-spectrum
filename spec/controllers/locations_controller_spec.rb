e0bd9d11ee2537e6b5ff3a1ab08dfef1ba75f055
Stuart Marquis <marquis@columbia.edu>
2015-03-18 16:03:57 -0400

    merge in location_map spec fixes

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -13,14 +13,13 @@ describe LocationsController, type: :controller do
   describe "build_markers" do
     it "should query the Library API" do
       api_query = "http://api.library.columbia.edu/query.json", {:params=>{:qt=>"location", :locationID=>"alllocations"}}
-      # expect(find(RestClient)).to receive(:get).with(api_query[0], api_query[1]).and_call_original
-      find(RestClient)
+      expect(RestClient).to receive(:get).with(api_query[0], api_query[1]).and_call_original
       controller.build_markers
     end
 
     it "assigns current marker" do
       controller.build_markers
-      expect(find(assigns(:current_marker_index))).to eq(0)
+      expect(assigns(:current_marker_index)).to eq(0)
     end
 
     it 'should not have a marker for location that does not have a map' do
@@ -33,7 +32,7 @@ describe LocationsController, type: :controller do
       cliolocs = Location.all.select{|loc| loc['library_code']}.map{|loc| loc['library_code']}.uniq
       cliolocs.each do |loc|
         unless((loc == "barnard-archives") || (loc == "butler-24") || (loc == "lehman-suite"))
-          expect(find(markers)).to match(loc)
+          expect(markers).to match(loc)
         end
       end
     end
