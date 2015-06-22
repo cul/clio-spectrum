40017c7bdb545d7193600203d7331db27d9b7692
Stuart Marquis <marquis@columbia.edu>
2015-06-22 13:13:16 -0400

    spec fixes

--- spec/features/voyager_api/test_collection_output_spec.rb
+++ spec/features/voyager_api/test_collection_output_spec.rb
@@ -6,12 +6,12 @@ describe 'collection output tests', js: true do
 
     visit catalog_path('8430339')
     within ('div#clio_holdings') do
-      page.should have_text('Butler Stacks (Enter at the Butler Circulation Desk)')
+      expect(page).to have_text('Butler Stacks (Enter at the Butler Circulation Desk)')
     end
 
     visit catalog_path('1052500')
     within ('div#clio_holdings') do
-      page.should have_text('MICROFLM FN 41 Library has: v.1851:Sept.-2003:Dec.14, 2004:Jan.')
+      expect(page).to have_text('MICROFLM FN 41 Library has: v.1851:Sept.-2003:Dec.14, 2004:Jan.')
     end
 
   end
