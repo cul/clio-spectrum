190d586e3672f00640c64864c64a6782251e172e
Stuart Marquis <marquis@columbia.edu>
2015-07-15 13:05:09 -0400

    Session Store back to ActiveRecord - too many CookieOverflow errors

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # The session holds the full set of search params (all facet values), plus the
 # previous_url.  Should we revert to ActiveRecord?  Or try file-store?
 
-Clio::Application.config.session_store :cookie_store, key: '_clio_session'
-# Clio::Application.config.session_store :active_record_store, :key => '_clio_session'
+# Clio::Application.config.session_store :cookie_store, key: '_clio_session'
+Clio::Application.config.session_store :active_record_store, :key => '_clio_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
