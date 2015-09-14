d90bcce40d3fb0e3702e1826e280f5173af475ac
Stuart Marquis <marquis@columbia.edu>
2015-09-14 16:25:19 -0400

    Try loading yml files with erb interpolation

--- config/initializers/aaa_load_app_config.rb
+++ config/initializers/aaa_load_app_config.rb
@@ -1,6 +1,11 @@
 begin
-  raw_config = File.read(Rails.root.to_s + '/config/app_config.yml')
-  loaded_config = YAML.load(raw_config)
+  # No interpolation
+  # raw_config = File.read(Rails.root.to_s + '/config/app_config.yml')
+  # loaded_config = YAML.load(raw_config)
+  # Interpolation
+  app_config_file = Rails.root.to_s + '/config/app_config.yml'
+  loaded_config = YAML.load(ERB.new(IO.read(app_config_file)).result) || {}
+
   all_config = loaded_config['_all_environments'] || {}
   env_config = loaded_config[Rails.env] || {}
   APP_CONFIG ||= all_config.merge(env_config)
@@ -15,4 +20,10 @@ raw_config = File.read(Rails.root.to_s + '/config/marc_display_fields.yml')
 marc_config = YAML.load(raw_config)
 MARC_FIELDS ||= marc_config
 
-SOLR_CONFIG ||= YAML.load(File.read(Rails.root.to_s + '/config/blacklight.yml'))
+# No interpolation
+# SOLR_CONFIG ||= YAML.load(File.read(Rails.root.to_s + '/config/blacklight.yml'))
+# Interpolation
+blacklight_config_file = Rails.root.to_s + '/config/blacklight.yml'
+SOLR_CONFIG = YAML.load(ERB.new(IO.read(blacklight_config_file)).result) || {}
+
+
