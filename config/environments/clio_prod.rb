bbb7e9b65afb7fcdcaa3c592201336845874cb14
Stuart Marquis <marquis@columbia.edu>
2016-05-03 10:29:24 -0400

    fix to skip mailing of errors for badly formed emails

--- config/environments/clio_prod.rb
+++ config/environments/clio_prod.rb
@@ -77,8 +77,7 @@ end
 #    :ignore_crawlers => %w{Googlebot bingbot}
 
 Clio::Application.config.middleware.use ExceptionNotification::Rack,
-  ignore_exceptions: ['Errno::EHOSTUNREACH'] + ExceptionNotifier.ignored_exceptions,
-  ignore_crawlers: %w(Googlebot bingbot archive.org_bot),
+  ignore_exceptions: ['Errno::EHOSTUNREACH', 'Mail::Field::ParseError'] + ExceptionNotifier.ignored_exceptions,
   ignore_crawlers: %w(Googlebot bingbot archive.org_bot Blogtrottr),
   email: {
     email_prefix: '[Clio Prod] ',
