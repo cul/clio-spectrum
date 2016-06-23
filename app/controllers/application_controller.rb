2f4887af3226c0a4c2ae668fbd3f3798a19ce232
Stuart Marquis <marquis@columbia.edu>
2016-06-23 13:30:34 -0400

    patch for change in Summon response format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -63,6 +63,8 @@ class ApplicationController < ActionController::Base
   end
 
   def condense_advanced_search_params
+    # Convert string representation to true empty hash
+    params['adv'] = {} if params['adv'] == '{}'
     new_hash = {}
     counter = 1
     (params['adv'] || {}).each_pair do |adv_field_number, attrs|
