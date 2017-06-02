811d2bca70e4812b00bf68a485c7cf9ecd2261f7
Stuart Marquis <marquis@columbia.edu>
2017-06-02 16:41:09 -0400

    add many more crons, all for clio_dev only for now

--- config/schedule.rb
+++ config/schedule.rb
@@ -13,9 +13,48 @@ set :job_template, "/usr/local/bin/mailifoutput -s ':subject (:environment)' :re
 
 # CLIO DEV
 if @environment == 'clio_dev'
+
+  # Still to do....
+  # - cleanup search records
+  # - cleanup sessions
+  # - sync library hours
+
+  # == BIBLIOGRAPHIC ==
+
+  # Nightly incremental
   every :day, at: '1am' do
     rake 'bibliographic:extract:process EXTRACT=incremental', subject: 'daily incremental'
   end
+  # Weekly cumulative, catch-up run in case we missed anything
+  every :sunday, at: '4am' do
+    rake 'bibliographic:extract:process EXTRACT=cumulative', subject: 'weekly cumulative'
+  end
+
+  #  ===  AUTHORITIES  ===
+
+  #  Add authority variants after each daily/weekly load 
+  every :day, at: '2am' do
+    rake 'authorities:add_to_bib:by_extract[incremental]', subject: 'daily authorities'
+  end
+  # Weekly catch-up against cumulative
+  every :sunday, at: '6am' do
+    rake 'authorities:add_to_bib:by_extract[cumulative]', subject: 'weekly authorities'
+  end
+
+  # == LAW ==
+
+  # Weekly full load of all Law records
+  every :sunday, at: '8am' do
+    rake 'bibliographic:extract:process EXTRACT=law', subject: 'law load'
+  end
+  # Weekly Delete of all stale Law records (3 week grace period)
+  # (TODO: this should definitely be coded into a rake task!)
+  every :sunday, at: '10am' do
+    command '/usr/bin/curl --silent "http://lito-solr-dev1.cul.columbia.edu:8983/solr/clio_dev/update?commit=true" -H "Content-Type: text/xml" --data-binary "<delete><query>timestamp:[* TO NOW/DAY-21DAYS] AND location_facet:Law</query></delete>"', subject: 'law purge'
+  end
+
+
+
 end
 
 
