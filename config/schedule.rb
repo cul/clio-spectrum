fa9803b37963ea3f024186b366a709436d362a03
Stuart Marquis <marquis@columbia.edu>
2017-06-09 11:08:04 -0400

    testing over, putting back to stable cron configs

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,35 +21,35 @@ if @environment != 'clio_prod'
 
   # == BIBLIOGRAPHIC ==
 
-  # # Nightly incremental
-  # every :day, at: '1am' do
-  #   rake 'bibliographic:extract:process EXTRACT=incremental', subject: 'daily incremental'
-  # end
-  # # Weekly cumulative, catch-up run in case we missed anything
-  # every :day, at: '4am' do
-  #   rake 'bibliographic:extract:process EXTRACT=cumulative', subject: 'weekly cumulative'
-  # end
-
-  # FULL - to be included as needed
-  every :day, at: '3:00pm' do
-    rake 'bibliographic:extract:process EXTRACT=full', subject: 'FULL'
+  # Nightly incremental
+  every :day, at: '1am' do
+    rake 'bibliographic:extract:process EXTRACT=incremental', subject: 'daily incremental'
   end
+  # Weekly cumulative, catch-up run in case we missed anything
+  every :sunday, at: '4am' do
+    rake 'bibliographic:extract:process EXTRACT=cumulative', subject: 'weekly cumulative'
+  end
+
+  # # FULL - to be included as needed
+  # every :foobarday, at: '3:00pm' do
+  #   rake 'bibliographic:extract:process EXTRACT=full', subject: 'FULL'
+  # end
 
   #  ===  AUTHORITIES  ===
 
-  # #  Add authority variants after each daily load 
-  # every :day, at: '2am' do
-  #   rake 'authorities:add_to_bib:by_extract[incremental]', subject: 'daily authorities'
-  # end
-  # # Weekly authority catch-up against cumulative
-  # every :day, at: '6am' do
-  #   rake 'authorities:add_to_bib:by_extract[cumulative]', subject: 'weekly authorities'
-  # end
+  #  Add authority variants after each daily load 
+  every :day, at: '2am' do
+    rake 'authorities:add_to_bib:by_extract[incremental]', subject: 'daily authorities'
+  end
+  # Weekly authority catch-up against cumulative
+  every :sunday, at: '6am' do
+    rake 'authorities:add_to_bib:by_extract[cumulative]', subject: 'weekly authorities'
+  end
 
   # == LAW ==
 
   # Weekly full load of all Law records
-  every :day, at: '8am' do
+  every :sunday, at: '8am' do
     rake 'bibliographic:extract:process EXTRACT=law', subject: 'law load'
   end
   # Weekly Delete of all stale Law records (3 week grace period)
@@ -57,8 +57,6 @@ if @environment != 'clio_prod'
   every :sunday, at: '10am' do
     command '/usr/bin/curl --silent "http://lito-solr-dev1.cul.columbia.edu:8983/solr/clio_dev/update?commit=true" -H "Content-Type: text/xml" --data-binary "<delete><query>timestamp:[* TO NOW/DAY-21DAYS] AND location_facet:Law</query></delete>"', subject: 'law purge'
   end
-
-
 
 end
 
