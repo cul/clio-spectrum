0b0c8c408671711f306d9d7e3dfd16ac9be6eba3
Stuart Marquis <marquis@columbia.edu>
2017-09-19 13:20:04 -0400

    move recap sftp private key to config directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/app_config.yml', 'config/blacklight.yml', 'public/robots.txt')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/app_config.yml', 'config/blacklight.yml', 'config/recapftp_rsa', 'public/robots.txt')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
