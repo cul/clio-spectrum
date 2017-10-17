f133d8f3653b7278db818c1491137bd65059d12a
Stuart Marquis <marquis@columbia.edu>
2017-10-17 13:07:41 -0400

    clio_app_prod deploy configuration file

---
+++ config/deploy/clio_app_prod.rb
@@ -0,0 +1,23 @@
+
+set :bundle_without,  [:development, :test, :clio_dev, :clio_test, :clio_prod]
+
+set :bundle_env_variables, { 
+  'http_proxy'  => 'http://lito-squid-prod1.cul.columbia.edu:3131',
+  'https_proxy' => 'https://lito-squid-prod1.cul.columbia.edu:3131'
+  }
+
+
+server 'clio-service-prod1.cul.columbia.edu', user: 'clioserv', roles: %w{app db web}
+
+# set :deploy_to, '/var/www/my_app_name'
+# set :deploy_to, '/opt/passenger/clio_prod'
+set :deploy_to, '/opt/app/clio_prod'
+
+# https://github.com/capistrano/rvm
+# set :rvm_type, :user                     # Defaults to: :auto
+# set :rvm_ruby_version, '2.0.0-p247'      # Defaults to: 'default'
+# set :rvm_custom_path, '~/.myveryownrvm'  # only needed if not detected
+set :rvm_ruby_version, 'clio_prod'
+
+
+
