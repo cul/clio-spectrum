98e30ff8f6eee7a4fb4b4a626e6388dd4d15256d
Stuart Marquis <stuartmarquis@gmail.com>
2018-08-23 11:52:23 -0400

    Factorybot update syntax

--- spec/factories/item_alert.rb
+++ spec/factories/item_alert.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
 
   factory :item_alert do
-    item_key '123'
-    alert_type 'alert'
-    message 'twas brillig'
-    author_id 1
-    source 'catalog'
+    item_key { '123' }
+    alert_type { 'alert' }
+    message { 'twas brillig' }
+    author_id { 1 }
+    source { 'catalog' }
   end
 
 end
