ebb24f6ac25430745db6d2a579b3214c2a518021
Stuart Marquis <stuartmarquis@gmail.com>
2019-04-10 21:42:39 -0400

    Safer google config init logic

--- config/initializers/google_api_client.rb
+++ config/initializers/google_api_client.rb
@@ -1,9 +1,11 @@
 
 require 'google/apis/core/base_service'
 
-Google::Apis.logger = Logger.new(STDERR)
-Google::Apis.logger.level = APP_CONFIG['google']['log_level'] || Logger::INFO
+google_config = APP_CONFIG['google'] || {}
 
-Google::Apis::RequestOptions.default.retries = APP_CONFIG['google']['api_retries'] || 2
+Google::Apis.logger = Logger.new(STDERR)
+Google::Apis.logger.level = google_config['log_level'] || Logger::INFO
+
+Google::Apis::RequestOptions.default.retries = google_config['api_retries'] || 2
 
 
