0800aef508f5f7b84f35dd4e79a4f996d9f39d33
Stuart Marquis <stuartmarquis@gmail.com>
2020-10-08 14:28:32 -0400

    NEXT-1700 - MARC XML Download bug

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -689,7 +689,7 @@ class CatalogController < ApplicationController
           stream.write solr_doc['marc_display'].
                          gsub(/<leader/, "\n    <leader").
                          gsub(/<controlfield/, "\n    <controlfield").
-                         gsub(/<\/?datafield/, "\n  <datafield").
+                         gsub(/<\/?datafield/, "\n  </datafield").
                          gsub(/<subfield/, "\n    <subfield").
                          gsub(/<\/record/, "\n</record")
           stream.write "\n"
