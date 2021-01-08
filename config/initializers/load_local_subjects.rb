0f52518fd2d50f41bb777477d7422ee396699ec7
Stuart Marquis <stuartmarquis@gmail.com>
2021-01-08 10:02:45 -0500

    cleanup initializer

--- config/initializers/load_local_subjects.rb
+++ config/initializers/load_local_subjects.rb
@@ -1,6 +1,5 @@
 
 # global mapping table from LOC subject heading to preferred local NNC subject heading
-LOCAL_SUBJECTS = {}
 
 # if ActiveRecord::Base.connection.table_exists?('local_subjects')
 #   LocalSubject.find_each do |local_subject|
@@ -9,8 +8,6 @@ LOCAL_SUBJECTS = {}
 # end
 
 local_subject_file = APP_CONFIG['extract_home'] + '/local_subjects/local_subjects.yml'
-if File.exist?(local_subject_file)
-  LOCAL_SUBJECTS = YAML.load( File.read(local_subject_file) )
-end
+LOCAL_SUBJECTS = File.exist?(local_subject_file) ? YAML.load(File.read(local_subject_file)) : {}
 
 
