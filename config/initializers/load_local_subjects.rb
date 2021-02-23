4af2fff28eb670dfbf79b5fef34d1f5323f8c4c1
Stuart Marquis <stuartmarquis@gmail.com>
2021-02-23 13:32:09 -0500

    NEXT-1601 - app_config toggle of NewSpeak

--- config/initializers/load_local_subjects.rb
+++ config/initializers/load_local_subjects.rb
@@ -1,14 +1,16 @@
 
-# global mapping table from LOC subject heading to preferred local NNC subject heading
+# NEXT-1601 - mapping table from LOC subject heading to preferred local NNC subject heading
 LOCAL_SUBJECTS = {}
 
-if ActiveRecord::Base.connection.table_exists?('local_subjects')
-  LocalSubject.find_each do |local_subject|
-    LOCAL_SUBJECTS[ local_subject.loc_subject ] = local_subject.nnc_subject
+# The app_config.yml toggle value controls whether the local-subjects database table 
+# is loaded into into a mapping table to be used in display and bib indexing.
+# The Authorities Load will populate the mapping table regardless.
+if APP_CONFIG['enable_local_subjects'] 
+  if ActiveRecord::Base.connection.table_exists?('local_subjects')
+    LocalSubject.find_each do |local_subject|
+      LOCAL_SUBJECTS[ local_subject.loc_subject ] = local_subject.nnc_subject
+    end
   end
 end
-
-# local_subject_file = APP_CONFIG['extract_home'] + '/local_subjects/local_subjects.yml'
-# LOCAL_SUBJECTS = File.exist?(local_subject_file) ? YAML.load(File.read(local_subject_file)) : {}
 
 
