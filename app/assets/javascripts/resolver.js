05b4d026543bab4bb3a5ad76587560354402fad3
Stuart Marquis <stuartmarquis@gmail.com>
2023-07-24 17:24:25 -0400

    Turn on Resolver-Rewrite console log debugging

--- app/assets/javascripts/resolver.js
+++ app/assets/javascripts/resolver.js
@@ -15,7 +15,7 @@ if (resolver_rewrite_javascript) {
 
     //console.log("found:" + url)
     if (url.startsWith(old_resolver_url)) {
-      //console.log("replacing:" + url)
+      console.log("replacing:" + url)
       new_url = url.replace(old_resolver_url, new_resolver_url)
       anchors[i].href = new_url;
     }
@@ -26,7 +26,7 @@ if (resolver_rewrite_javascript) {
   endTime = new Date();
   timeElapsed = endTime - startTime;
 
-  //console.log("Time Elapsed (s):" + (timeElapsed/1000) );
+  console.log("Time Elapsed (s):" + (timeElapsed/1000) );
 
 }
 
