f74e79eaffe9a020e578d65a35935e8714b406b0
Stuart Marquis <stuartmarquis@gmail.com>
2024-03-25 12:54:04 -0400

    Cleanup deployment files for "clio_batch_X"

--- config/deploy/clio_batch_prod.rb
+++ config/deploy/clio_batch_prod.rb
@@ -1,8 +1,8 @@
 
 set :bundle_without, %w(development test clio_dev clio_test clio_prod).join(' ')
 
-# set :bundle_env_variables, 'http_proxy' => 'http://squid.cul.columbia.edu:3131',
-#                            'https_proxy' => 'https://squid.cul.columbia.edu:3131'
+# set :bundle_env_variables, 'http_proxy' => 'http://lito-squid-prod1.cul.columbia.edu:3131',
+#                            'https_proxy' => 'https://lito-squid-prod1.cul.columbia.edu:3131'
 
 set :bundle_env_variables, 'http_proxy' => 'http://squid.cul.columbia.edu:3131'
 
@@ -10,10 +10,10 @@ server 'clio-service-prod2.cul.columbia.edu', user: 'clioserv', roles: %w(app db
 
 # set :deploy_to, '/var/www/my_app_name'
 # set :deploy_to, '/opt/passenger/clio_prod'
-set :deploy_to, '/opt/clio/batch_dev'
+set :deploy_to, '/opt/clio/batch_prod'
 
 # https://github.com/capistrano/rvm
 # set :rvm_type, :user                     # Defaults to: :auto
 # set :rvm_ruby_version, '2.0.0-p247'      # Defaults to: 'default'
 # set :rvm_custom_path, '~/.myveryownrvm'  # only needed if not detected
-set :rvm_ruby_version, 'clio_dev'
+set :rvm_ruby_version, 'clio_test'
