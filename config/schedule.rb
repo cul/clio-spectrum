5304791927c8c97e5624fb5f6777c1748b67e5b1
Stuart Marquis <stuartmarquis@gmail.com>
2024-06-13 12:19:21 -0400

    schedule.rb - try offsets for Dev and Test cron jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -24,16 +24,25 @@ set :job_template, "mailifoutput -s ':subject (:environment)' :recipient  /bin/b
 # Our batch processing environments
 if %w(clio_batch_dev clio_batch_test clio_batch_prod).include?(@environment)
 
-  set :path, '/opt/clio/batch_dev/current'    if @environment.eql?('clio_batch_dev')
-  set :path, '/opt/clio/batch_test/current'   if @environment.eql?('clio_batch_test')
-  set :path, '/opt/clio/batch_prod/current'   if @environment.eql?('clio_batch_prod')
+  if @environment.eql?('clio_batch_dev')
+    set :path, '/opt/clio/batch_dev/current'
+    offset = 'two hours after '
+  end
+  if @environment.eql?('clio_batch_test')
+    set :path, '/opt/clio/batch_test/current'
+    offset = 'one hour after'
+  end
+  if @environment.eql?('clio_batch_prod')
+    set :path, '/opt/clio/batch_prod/current'
+    offset = ''
+  end
 
 
   # == BIBLIOGRAPHIC ==
   # Run all daily ingest tasks, together within one rake task.
   # The daily clio-extract needs to complete before this.
   # That begins at 10pm and usually completes by midnight.
-  every :day, at: '1am' do
+  every :day, at: offset + '1am' do
     rake 'bibliographic:daily', subject: 'bibliographic:daily'
   end
 
@@ -49,22 +58,22 @@ if %w(clio_batch_dev clio_batch_test clio_batch_prod).include?(@environment)
   end
 
   # == RECAP ==
-  every :day, at: '7am' do
+  every :day, at: offset + '7am' do
     rake 'recap:delete_new[2]', subject: 'recap:delete_new'
   end
-  every :day, at: '8am' do
+  every :day, at: offset + '8am' do
     rake 'recap:ingest_new[2]', subject: 'recap:ingest_new'
   end
 
   # == AUTHORITIES ==
   # Run all daily ingest tasks, together within one rake task
-  every :day, at: '9am' do
+  every :day, at: offset + '9am' do
     rake 'authorities:daily', subject: 'authorities:daily'
   end
 
   # == LAW ==
   # Weekly full load of all Law records
-  every :sunday, at: '10am' do
+  every :sunday, at: offset + '10am' do
     rake 'bibliographic:extract:process EXTRACT=law', subject: 'law load'
   end
 
