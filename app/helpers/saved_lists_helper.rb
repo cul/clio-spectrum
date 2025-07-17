ba1d2d86c4cf0f4b75b7a63f11fa978ab9f3859a
Stuart Marquis <stuartmarquis@gmail.com>
2025-07-17 14:50:03 -0400

    Add No-Articles blurb to Saved List

--- app/helpers/saved_lists_helper.rb
+++ app/helpers/saved_lists_helper.rb
@@ -14,4 +14,18 @@ module SavedListsHelper
       end
     html.html_safe
   end
+  
+  def no_articles_blurb()
+    
+    icon = content_tag(:span, nil, class: 'glyphicon glyphicon-info-sign')
+    
+    zotero = link_to "Zotero", "https://library.columbia.edu/services/citation-management.html"
+    
+    message = "&nbsp; Articles+ content previously saved to CLIO Saved Lists is regrettably no longer available due to a vendor platform change. We recommend the use of third party citation management software such as <strong>#{zotero}</strong> as an alternative for those interested in managing non-Catalog citations."
+    
+    html = '<div class="alert alert-info" style="padding:6px">' + icon + message + '</div>'
+    
+    return html.html_safe
+  end
+  
 end
