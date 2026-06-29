a51ff3433a692da2956dcc73e80e61ed433f8eda
Stuart Marquis <stuartmarquis@gmail.com>
2026-06-29 14:43:52 -0400

    Better sensitive-value filtering using regex

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,5 +2,7 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [
-  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
+  # use a regex to catch case-insensitive substring patterns
+  /passw|secret|token|_key|crypt|salt|certificate|otp|ssn|access_key|credential/i
+  
 ]
